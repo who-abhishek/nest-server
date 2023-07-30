083dfc143238298c634895237d71cf8e4edb8c6d
who-abhishek <abhishekyadav28@outlook.com>
2023-07-30 12:02:34 +0530

    test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [UserController],
+  controllers: [UserController],
   providers: [],
 })
 export class AppModule {}
